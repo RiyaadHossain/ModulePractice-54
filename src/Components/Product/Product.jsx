8bc4699e676b96581d93c1e783fad7797050fa23
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-02 11:20:51 +0600

    Data fetched and components are Created

---
+++ src/Components/Product/Product.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Product = ({product}) => {
+    return (
+        <div>
+            <h3>{product.title}</h3>
+        </div>
+    );
+};
+
+export default Product;
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import Product from "../Product/Product";
 
 const Shop = () => {
-    return (
-        <div>
-            <h1>Shopping from here.</h1>
-        </div>
-    );
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => setProducts(json));
+  }, []);
+  return (
+    <div>
+          <div className="shop-container">
+          {
+              products.map(product => <Product key={product.id} product={product}/>)
+          }
+          </div>
+    </div>
+  );
 };
 
 export default Shop;
